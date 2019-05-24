fa6cf9c912a0a0bb9cf8122a42fb89ccc56a139d
shembull <shembull@me.com>
2019-05-24 12:20:42 +0200

    Added functionality to the buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,22 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'My first Sample Project';
-    buttonList = [
-        {title: 'Hey', text: 'Hier'},
-        {title: 'those', text: 'sind'},
-        {title: 'are', text: 'ein'},
-        {title: 'some', text: 'paar'},
-        {title: 'Tooltips', text: 'Buttons'}
-        ];
-
-  public changeTitle() {
-      this.title += 'lol';
-  }
-
-  public dontCopy(element: string): void {
-      alert('Dont copy my ' + element + '!');
-  }
+    title = 'My first Sample Project';
+        buttonList = [
+            {title: 'Hey', text: 'Hier', clicked: false},
+            {title: 'those', text: 'sind', clicked: false},
+            {title: 'are', text: 'ein', clicked: false},
+            {title: 'some', text: 'paar', clicked: false},
+            {title: 'Tooltips', text: 'Buttons', clicked: false}
+            ];
+    public changeTitle() {
+          this.title += 'lol';
+        // tslint:disable-next-line:prefer-for-of
+          for (let i = 0; i < this.buttonList.length; i++) {
+              this.buttonList[i].clicked = false;
+        }
+    }
+    public dontCopy(element: string): void {
+          alert('Dont copy my ' + element + '!');
+    }
+    public disableButton(button) {
+        button.clicked ? button.clicked = false : button.clicked = true;
+    }
+    public log(msg) {
+        console.log(msg);
+    }
 }
-
-
