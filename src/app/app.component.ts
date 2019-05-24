695e243fe0d8db36e4c84dc11e0f45a73ca63950
shembull <shembull@me.com>
2019-05-24 09:53:37 +0200

    Added custom module class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'My first Sample Project';
-  fname = 'Test';
-  lname = 'Even more Test';
 }
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material';
-import {FormsModule} from '@angular/forms';
+import {MaterialModule} from './angular-material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatButtonModule,
-    FormsModule
+      BrowserModule,
+      AppRoutingModule,
+      BrowserAnimationsModule,
+      MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
