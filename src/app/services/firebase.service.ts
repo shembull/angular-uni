1d2b5592211465956c5489126f5aaf575b4468a5
shembull <shembull@me.com>
2019-05-30 13:42:35 +0200

    added firebase-firestore module, config and imports

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import {AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument} from '@angular/fire/firestore';
+import {User} from '../classes/user';
+import {config} from '../app.config';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirebaseService {
+    users: AngularFirestoreCollection<User>;
+    private userDoc: AngularFirestoreDocument<User>;
 
-  constructor() { }
+  constructor(private db: AngularFirestore) {
+      this.users = db.collection<User>(config.collection_endpoint)
+  }
+  addUser(user: User) {
+      this.users.add(user);
+  }
 }
