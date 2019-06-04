f24630550f7341180555028c00909c4b2426b14b
shembull <46959681+shembull@users.noreply.github.com>
2019-06-04 20:51:41 +0200

    updated id calculation parameters

--- src/app/classes/user.ts
+++ src/app/classes/user.ts
@@ -5,6 +5,7 @@ export class User implements UserInterface {
     public fname: string;
     public lname: string;
     public phone: string;
+    public mail: string;
     public id: string;
     constructor(fname: string, lname: string, phone: string) {
         this.fname = fname;
@@ -12,8 +13,8 @@ export class User implements UserInterface {
         this.phone = phone;
         this.id = sha256(this.fname + this.lname + this.phone);
     }
-    static getID(fname: string, lname: string, phone: string): string {
-        return sha256(fname + lname + phone);
+    static getID(user: UserInterface): string {
+        return sha256(user.fname + user.lname + user.phone + user.mail);
     }
     updateID(): void {
         this.id = sha256(this.fname + this.lname + this.phone);
