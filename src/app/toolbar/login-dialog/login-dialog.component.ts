4bbbe6fe4508f3075938e3e45c86db642aa35675
shembull <46959681+shembull@users.noreply.github.com>
2019-06-17 10:47:23 +0200

    added form control to user authentication

--- src/app/toolbar/login-dialog/login-dialog.component.ts
+++ src/app/toolbar/login-dialog/login-dialog.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {MatDialogRef} from '@angular/material';
 import {AuthService} from '../../services/auth.service';
+import {UserErrorStateMatcher} from '../../start/user-add/user-add.component';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login-dialog',
@@ -8,6 +10,14 @@ import {AuthService} from '../../services/auth.service';
   styleUrls: ['./login-dialog.component.css']
 })
 export class LoginDialogComponent implements OnInit {
+    emailFormControl = new FormControl('', [
+        Validators.email,
+        Validators.required,
+    ]);
+    passwordFormControl = new FormControl('', [
+        Validators.required,
+    ]);
+    matcher = new UserErrorStateMatcher();
 
     constructor(
         public dialogRef: MatDialogRef<LoginDialogComponent>,
